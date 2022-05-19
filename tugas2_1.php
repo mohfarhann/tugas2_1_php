2d96602104451193b3924d0cdf51614824411c7f
mohfarhann <90471536+mohfarhann@users.noreply.github.com>
2022-05-19 14:03:03 +0700

    Add files via upload

---
+++ tugas2_1.php
@@ -0,0 +1,12 @@
+<?php 
+	$x = 12;
+	$y = 36;
+
+	$z = 24;
+	$c = $y / 2;
+
+	echo "<h5>$y Hari = $x Pekerja</h5>";
+	echo "<h5> X Hari = $z Pekerja</h5>";
+	echo "<h5>X = $c</h5>";
+	echo "<h5>Jawaban : $c </h5>";
+?>
